08a03fbd229a54f717ea1d5ba50d968bb1122228
Max Tobias Weber <tobias.weber@wheregroup.com>
2021-12-13 12:36:13 +0100

    Fix HamburgerMenu CSS Grid

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,13 +1,15 @@
 import React from "react";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import { IconButton, Grid } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const HamburgerMenu = (props) => {
+  const mediaIsMobile = useMediaQuery("(max-width:900px)");
   return (
     <Grid
       item
       md={2}
-      xs={12}
+      xs={mediaIsMobile ? 4 : 12}
       style={{
         display: "flex",
         justifyContent: "flex-end",
