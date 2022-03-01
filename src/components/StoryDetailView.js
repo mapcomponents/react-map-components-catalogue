4fbaccfc1317e740fbedfd4ad9c0b1e625a11ab1
Max Tobias Weber <tobias.weber@wheregroup.com>
2022-03-01 16:49:22 +0100

    fix component detail description

--- src/components/StoryDetailView.js
+++ src/components/StoryDetailView.js
@@ -259,7 +259,7 @@ function StoryDetailView(props) {
           }}>
             <h3>{t("description")}</h3>
             <Divider variant="fullWidth" sx={{ bgcolor: theme.palette.secondary.main }}></Divider>
-            <p>{description}</p>
+            <div dangerouslySetInnerHTML={description} />
           </div>
 
 
